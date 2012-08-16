ecdf79d83b748ebd9e1ec69a8ba9ac08159be543
iven <ivenvd@gmail.com>
2012-08-16 21:13:43 +0800

    Use partial templates.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -46,7 +46,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @cart, notice: 'Book successfully added.' }
+        format.html { redirect_to store_url }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
@@ -79,7 +79,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to cart, notice: 'Book Removed' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+LineItem.delete_all
+Cart.delete_all
 Product.delete_all
 
 Product.create(:title => 'Web Design for Developers',
