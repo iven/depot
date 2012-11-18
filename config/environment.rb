9fc6f53f1c354a33686fbfb631c74e8adfddeaf4
iven <ivenvd@gmail.com>
2012-11-18 17:36:41 +0800

    Add email sending.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "www.kissuki.com",
+    authentication:       "plain",
+    user_name:            "ivenvd",
+    password:             "iven9988",
+    enable_starttls_auto: true
+  }
+end
